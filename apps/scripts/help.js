aa53a5e02a935bf97ea7ea886816de3a61563b30
miguel2729 <miguelbc1407@gmail.com>
2026-01-09 16:18:19 -0300

    sem commit

--- apps/scripts/help.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", function(event) {
-  // Verifica se a tecla pressionada é "h" (minúscula)
-  if (event.key.toLowerCase() === "h") {
-    parent.postMessage("abrir manual.html", "*");
-  }
-});
